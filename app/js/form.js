41c9f0179242573dae317c0af15fef11794101c3
mjarnicki <37507193+mjarnicki@users.noreply.github.com>
2020-11-01 21:54:29 +0100

    formularz prawie skończony. Zostało przełączanie miedzy elementami.

--- app/js/form.js
+++ app/js/form.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    let hidePasswordFlag = false
+    let hidePasswordFlag = false;
 
-    document.querySelector('.form__show-password').addEventListener('click', event => {
+    document.querySelector('.form__show-password-button').addEventListener('click', event => {
 
         event.preventDefault()
-        const showPasswordbutton = event.target.closest('.form__show-password')
+        const showPasswordbutton = event.target.closest('.form__show-password-button')
         const passwordInput = showPasswordbutton.previousElementSibling 
 
         if(hidePasswordFlag) {
@@ -18,7 +18,20 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         hidePasswordFlag = !hidePasswordFlag
-    })
+    });
+
+    document.querySelector('#login-submit').addEventListener('click', validateForm)
+    document.querySelector('#register-submit').addEventListener('click', validateForm)
 
 
 });
+
+function validateForm(event) {
+    event.preventDefault();
+    const formContainer = event.target.parentNode;
+    const formValidity = formContainer.checkValidity();
+
+    console.log(formValidity);
+
+    formContainer.classList.add('form__validate')
+}
