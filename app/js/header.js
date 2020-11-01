f67d37e0df5840b382e09cface94b46b0b040f14
mjarnicki <37507193+mjarnicki@users.noreply.github.com>
2020-11-01 12:59:12 +0100

    dodany focus trap dla mobilki

--- app/js/header.js
+++ app/js/header.js
@@ -12,10 +12,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // toggle HamburgerMenu events
     
-    hamburgerIcon.addEventListener('click', toggleMobileMenu);
+    hamburgerIcon.addEventListener('click', () => {
+      toggleMobileMenu()
 
-    hamburgerIcon.addEventListener('keypress  ', event => {
-      if(event.keyCode == 32 || event.keyCode == 13) toggleMobileMenu()
+
     });
 
     // move to page section events for mobile
@@ -42,14 +42,12 @@ document.addEventListener("DOMContentLoaded", function() {
       });
 
       item.addEventListener('keypress', event => {
-        if(event.keyCode == 32 || event.keyCode == 13) toggleMobileMenu()
+        if(event.keyCode == 32 || event.keyCode == 13)
         scrollToPageSection(event, item, '-148px')
       });
     })
 
     // mark active element depending on scroll position TODO
-
- 
 
     // functions
 
@@ -63,21 +61,53 @@ document.addEventListener("DOMContentLoaded", function() {
           if(el.getAttribute('tabindex')) el.removeAttribute('tabindex')
           else el.setAttribute("tabindex", -1)
         })
+        trapFocus(parent)
     }
-
-    function scrollToPageSection(event, item, offset) {
-      
-      event.preventDefault()
-
-      const targetId = item.getAttribute('href'); 
-      const target = document.querySelector(targetId)
-      const pos = target.style.position;
-      const top = target.style.top;
-
-      target.style.position = 'relative';
-      target.style.top = offset;
-      target.scrollIntoView({ behavior: 'smooth'});
-      target.style.top = top;
-      target.style.position = pos;
-    }
+    
 });
+
+function scrollToPageSection(event, item, offset) {
+      
+  event.preventDefault()
+
+  const targetId = item.getAttribute('href'); 
+  const target = document.querySelector(targetId)
+  const pos = target.style.position;
+  const top = target.style.top;
+
+  target.style.position = 'relative';
+  target.style.top = offset;
+  target.scrollIntoView({ behavior: 'smooth'});
+  target.style.top = top;
+  target.style.position = pos;
+}
+
+function trapFocus(element) {
+  const focusableEls = element.querySelectorAll('a[href]:not([focus-trap-disabled]), button:not([focus-trap-disabled])');
+  const firstFocusableEl = focusableEls[0];  
+  const lastFocusableEl = focusableEls[focusableEls.length - 1];
+  const KEYCODE_TAB = 9;
+
+  console.log(element);
+
+      element.addEventListener('keydown', function(e) {
+        var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+        
+        if (!isTabPressed) { 
+            return; 
+        }
+
+        if ( e.shiftKey ) /* shift + tab */ {
+            if (document.activeElement === firstFocusableEl) {
+              
+              lastFocusableEl.focus();
+                e.preventDefault();
+            }
+        } else /* tab */ {
+            if (document.activeElement === lastFocusableEl) {
+                firstFocusableEl.focus();
+                e.preventDefault();
+            }
+          }
+      });
+  }
